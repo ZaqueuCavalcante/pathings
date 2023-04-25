ea782c545253d5ced6b1b28677a274bb77046cbe
ZaqueuCavalcante <zaqueudovale@gmail.com>
2023-04-25 14:26:47 -0300

    Remove router

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter, Route, Routes } from "react-router-dom";
 
 const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
