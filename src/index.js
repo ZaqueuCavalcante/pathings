231e78f2a729865aa0e4f5dfacc8fdc902b2adc5
ZaqueuCavalcante <zaqueudovale@gmail.com>
2023-04-25 14:20:10 -0300

    Add router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
